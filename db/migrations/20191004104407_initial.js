66af1176e60e55416b66cbe5f3d29540f3348ead
Emily <lalonde0902@gmail.com>
2019-10-06 11:37:35 -0600

    Add migrations for movies and directors

--- db/migrations/20191004104407_initial.js
+++ db/migrations/20191004104407_initial.js
@@ -1,8 +1,29 @@
-
 exports.up = function(knex) {
-  
+  return Promise.all([
+    knex.schema.createTable('directors', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('country');
+      table.integer('age');
+
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('movies', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.integer('releaseDate');
+      table.string('director');
+      table.integer('directorsId').unsigned();
+      table.foreign('directorsId').references('directorId');
+
+      table.timestamps(true, true)
+    })
+  ])
 };
 
 exports.down = function(knex) {
-  
+  return Promise.all([
+    knex.schema.dropTable('directors'),
+    knex.schema.dropTable('movies')
+  ])
 };
